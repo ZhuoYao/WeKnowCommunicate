58109debae6caea2617916644a981ec8a7c88a7d
g39088902 <g39088902@gmail.com>
2020-03-02 19:56:00 +0800

    usercenter test

--- pages/usercenter/usercenter.js
+++ pages/usercenter/usercenter.js
@@ -1,6 +1,8 @@
 // pages/usercenter/usercenter.js
+//获取应用实例
+var app = getApp();
+var Loger = require("../../utils/Loger.js");
 Page({
-
   /**
    * 页面的初始数据
    */
@@ -15,6 +17,14 @@ Page({
     var that = this
     that.setData({
       nums: [15, 23, 77, 64]
+    })
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function (userInfo) {
+      //更新数据
+      that.setData({
+        userInfo: userInfo,
+        uservector:"/images/weKnow-vector.png"
+      })
     })
   },
 
